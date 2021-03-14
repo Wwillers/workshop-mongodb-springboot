a2d711b49ba52c55f15d8c4e176b381cd95abf14
William Willers <w.willers@outlook.com>
2021-03-14 19:11:56 -0400

    Insercao de User

--- src/main/java/com/wwillers/workshop/services/UserService.java
+++ src/main/java/com/wwillers/workshop/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.wwillers.workshop.domain.User;
+import com.wwillers.workshop.dto.UserDTO;
 import com.wwillers.workshop.repository.UserRepository;
 import com.wwillers.workshop.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
   public User findById(String id) {
     Optional<User> obj = repo.findById(id);
     return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+  }
+
+  public User insert(User obj) {
+    return repo.insert(obj);
+  }
+
+  public User fromDTO(UserDTO objDto) {
+    return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
   }
 
 }
