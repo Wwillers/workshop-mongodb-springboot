6eb7a515224267f51823c442a21dbf02a1ae63a2
William Willers <w.willers@outlook.com>
2021-03-16 21:30:21 -0400

    Acrescentando comentarios aos Posts

--- src/main/java/com/wwillers/workshop/config/Instantiation.java
+++ src/main/java/com/wwillers/workshop/config/Instantiation.java
@@ -7,6 +7,7 @@ import java.util.TimeZone;
 import com.wwillers.workshop.domain.Post;
 import com.wwillers.workshop.domain.User;
 import com.wwillers.workshop.dto.AuthorDTO;
+import com.wwillers.workshop.dto.CommentDTO;
 import com.wwillers.workshop.repository.PostRepository;
 import com.wwillers.workshop.repository.UserRepository;
 
@@ -41,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
     Post firstPost = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para SP, abraços!",
         new AuthorDTO(maria));
     Post secondPost = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Estou muito feliz", new AuthorDTO(maria));
+
+    CommentDTO firstComment = new CommentDTO("Boa viagem!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+    CommentDTO secondComment = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+    CommentDTO thirdComment = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+    firstPost.getComments().addAll(Arrays.asList(firstComment, secondComment));
+    secondPost.getComments().addAll((Arrays.asList(thirdComment)));
 
     postRepository.saveAll(Arrays.asList(firstPost, secondPost));
 
