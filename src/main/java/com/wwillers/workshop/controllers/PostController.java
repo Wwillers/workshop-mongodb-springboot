ba62e0cf25165fdc0c160f06e2ce0cafed6b5649
William Willers <w.willers@outlook.com>
2021-03-17 19:09:34 -0400

    Consulta simples com query methods

--- src/main/java/com/wwillers/workshop/controllers/PostController.java
+++ src/main/java/com/wwillers/workshop/controllers/PostController.java
@@ -1,13 +1,13 @@
 package com.wwillers.workshop.controllers;
 
+import com.wwillers.workshop.controllers.util.URL;
 import com.wwillers.workshop.domain.Post;
 import com.wwillers.workshop.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,6 +20,13 @@ public class PostController {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
